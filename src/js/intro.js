c89e9ae8639adeecb326040aabe2f4078c21ea17
relbeits_ <aaron.relbeits@gmail.com>
2014-07-30 16:31:09 -0300

    Change log - v140730-RES04a
    --------------------------
    //Changes/
    
    - Changed the entire code to work
    
    --------------------------
    //Author Notes/
    
    Code by @dragonslovetacos in stackoverflow website.

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,18 +1,7 @@
-var blinker = function({
-    $('.loading').fadeOut(500);
-    $('.loading').fadeIn(500);
-});
-
-setInterval(blinker, 1000);
-
-var vanish = function(){
-    $(document).ready( function(){
-        $('.intro').animate({
-            top: '-100%'
-        }, 4000);
-        $('.body').animate({
-            left: '-100%'
-        }, 4000);
+function blink() {
+    $('.loading').fadeToggle('slow', function () {
+        // Done.
     });
+}
 
-$(document).ready(vanish);
+setInterval(blink, 500);
